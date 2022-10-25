2ce3b9c4aa6ac2ddcd1ae35271b3d2ba62c25909
Victor <Victor@Vinces-MacBook-Air.local>
2022-10-24 21:15:27 -0700

    Users mock events

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// mock array of projects
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+]
+
+// index
+app.get('/', (req, res) => {
+  res.render('events-index', {events: events });
+})
+
 // Choose port to listen
 const port = process.env.PORT || 3000;
 
