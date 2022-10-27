142d5220d018826933f7143a87ad03bd9c00e7ca
Victor <Victor@Vinces-MacBook-Air.local>
2022-10-27 10:57:54 -0700

    Users can create events

--- app.js
+++ app.js
@@ -6,30 +6,46 @@ const app = express()
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const bodyParser = require('body-parser');
 
 // use "main" as our default layout
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!'});
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Tell app to say hello world
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const models = require('./db/models');
 
 // mock array of projects
 var events = [
   { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
 ]
 
 // index
 app.get('/', (req, res) => {
-  res.render('events-index', {events: events });
+  models.Event.findAll({ order: [['createdAt', 'DESC']]}).then(events => {
+    res.render('events-index', { events: events });
+  })
 })
+
+
+// NEW
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
+})
+
+// CREATE
+app.post('/events', (req, res) => {
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+
 
 // Choose port to listen
 const port = process.env.PORT || 3000;
